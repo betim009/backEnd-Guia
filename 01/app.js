060450c71836b7113032a04d420d259d0bbe8bbe
Alberto <betim009@gmail.com>
2024-02-08 17:17:08 -0300

    alterando res da rota city

--- 01/app.js
+++ 01/app.js
@@ -9,15 +9,18 @@ app.use(cors());
 
 app.get('/city', async (req, res) => {
     const [result] = await connection.execute('SELECT * FROM city');
-    console.log(result);
 
-    res.json(result);
+    const data = result.map((e) => ({
+        cityId: e.city_id,
+        city: e.city,
+    }));
+    
+    res.status(200).json(data);
 });
 
 app.get('/country', async (req, res) => {
     const [result] = await connection.execute('SELECT * FROM country');
     res.json(result);
 });
-
 
 module.exports = app;
